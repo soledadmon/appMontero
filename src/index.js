b2506aa0a37d582fd9f677e9a2a46225af30abfa
Soledad <soledadmon@gmail.com>
2021-10-13 23:37:30 -0300

    Comit iniciar

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/*import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();*/
+
+const Elemento = () => <p>"Hola Mundo"</p>
+ReactDOM.render(
+  <Elemento/>,
+  document.getElementById('root')
+);
