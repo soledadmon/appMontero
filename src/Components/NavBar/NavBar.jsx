a94d920d0d8d5a9e53f24d569b645a16abc3a201
Soledad <soledadmon@gmail.com>
2022-01-10 21:51:10 -0300

    cambios 10/01

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './NavBar.css';
+
+export default function NavBar(){
+    return (
+        <>
+            <div> 
+            <div id="barra">
+                <ul>
+                    <li><a href="#">Elemento 1</a></li>
+                    <li><a href="#">Elemento 2</a></li>
+                    <li><a href="#">Elemento 3</a></li>
+                    <li><a href="#">Elemento 4</a></li>
+                </ul>
+            </div>
+            </div>
+        </>
+    )
+}
+
+
+
+
