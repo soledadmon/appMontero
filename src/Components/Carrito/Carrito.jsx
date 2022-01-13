d9483c3e3616a4d20137762b4d7eff3a5788b745
Soledad <soledadmon@gmail.com>
2022-01-12 23:48:59 -0300

    actualizacion 1 del 12/01/2022

---
+++ src/Components/Carrito/Carrito.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import carrito from '../../Imagenes/carrito.png';
+import './Carrito.css';
+
+export default function Carrito(){
+    return (
+        <>      
+            <img id = "carrito" src={carrito}/>
+                        
+        </>
+    )
+}
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {Component} from "react";
 import './NavBar.css';
-
+import  Carrito from '../Carrito/Carrito';
+ 
 export default function NavBar(){
     return (
-        <> 
+        <>  
             <div> 
+            
             <div id="barra">
                 <ul>
                     <li><a href="#">Elemento 1</a></li>
                     <li><a href="#">Elemento 2</a></li>
                     <li><a href="#">Elemento 3</a></li>
                     <li><a href="#">Elemento 4</a></li>
+                    <li><Carrito/></li>
                 </ul>
             </div>
             </div>
