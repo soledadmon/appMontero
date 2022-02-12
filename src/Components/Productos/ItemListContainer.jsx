d11c5fb13b76c033549edd84a47f289828fa5311
Soledad <soledadmon@gmail.com>
2022-02-11 22:56:36 -0300

    11/02 actualizacion

--- src/Components/Productos/ItemListContainer.jsx
+++ src/Components/Productos/ItemListContainer.jsx
@@ -1,16 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route, useParams, Link } from 'react-router-dom';
 import ItemList from './ItemList.jsx';
-import vainilla from '../../Imagenes/vainilla.jpg';
-import frutilla from '../../Imagenes/frutilla.jpg';
-import dulceDeLecheGranizado from '../../Imagenes/dulceDeLecheGranizado.jpg';
-import chocolate from '../../Imagenes/chocolate.jpg';
-import PostreAlmendrado from '../../Imagenes/PostreAlmendrado.jpg';
-import PostreDDL from '../../Imagenes/PostreDDL.jpg';
-import PostreFantasia from '../../Imagenes/PostreFantasia.jpg';
-import promo2_1 from '../../Imagenes/promo2_1.jpg';
-import promo4s from '../../Imagenes/promo4s.jpg';
-import promokg from '../../Imagenes/promokg.jpg';
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { getFirestore } from "../../firebase/firebase";
@@ -21,10 +11,18 @@ export default function ItemListContainer() {
 
     const [items, setItems] = useState();
     const [llegoPromesa, setLlegoPromesa] = useState(false);
-
+    const base = getFirestore();
+   
+console.log(id);
     useEffect(() => {
-        const base = getFirestore();
-        const coleccionItems = base.collection("items")
+
+        let coleccionItems = base.collection("items") 
+    
+        if (id > 0) {
+            
+            coleccionItems = coleccionItems.where('categoriaId', '==', 1 )
+            
+        }
 
         coleccionItems.get().then((querySnapShot) => {
             if (querySnapShot.size === 0) {
@@ -42,31 +40,14 @@ export default function ItemListContainer() {
         }).catch(err => {
             console.log(err);
         })
-    }, []);
+    }, id);
 
-
-    function evaluo(items) {
-
-        if (id > 0) {
-
-            let arrayAux = new Array();
-            items.forEach(element => {
-                if (element.categoriaId == id) {
-
-                    arrayAux.push(element);
-                }
-
-            });
-         items = arrayAux;
-        }
-        return items;
-    }
 
     return (
         <> {
             (llegoPromesa) ?
 
-                <ItemList items={evaluo(items)} />
+                <ItemList items={items} />
                 : <>Loading..</>
         }
         </>
